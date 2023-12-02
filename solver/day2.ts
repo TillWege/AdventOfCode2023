c6472a3ce9cf0ecc2882617f03defd0ea9f18834
TillWege <tillwege@gmail.com>
2023-12-03 00:27:53 +0100

    day 2 part 1

---
+++ solver/day2.ts
@@ -0,0 +1,52 @@
+import { Solver } from "../index";
+import { readFileSync } from "fs";
+
+let input: string[];
+
+const solver: Solver = {
+  init: () => {
+    input = readFileSync("./input/day2.txt", "utf-8").split(/\r?\n/);
+  },
+  solvePart1: () => {
+    const maxRed = 12;
+    const maxGreen = 13;
+    const maxBlue = 14;
+    let sum = 0;
+
+    input.forEach((line) => {
+      let id = parseInt(line.slice(5, line.indexOf(":")));
+
+      const rounds = line.slice(line.indexOf(": ") + 2).split("; ");
+      rounds.forEach((round) => {
+        const showings = round.split(", ");
+        showings.forEach((show) => {
+          const parts = show.split(" ");
+          const count = parseInt(parts[0]);
+          const color = parts[1];
+
+          if (color == "red") {
+            if (count > maxRed) {
+              id = 0;
+            }
+          } else if (color == "green") {
+            if (count > maxGreen) {
+              id = 0;
+            }
+          } else if (color == "blue") {
+            if (count > maxBlue) {
+              id = 0;
+            }
+          }
+        });
+      });
+      sum += id;
+    });
+
+    return sum.toString();
+  },
+  solvePart2: () => {
+    return "";
+  },
+};
+
+export default solver;
