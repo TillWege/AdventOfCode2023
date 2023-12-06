8c1cca2e992b46a6829fb61980e8c15cc1dd9538
TillWege <tillwege@gmail.com>
2023-12-06 08:28:40 +0100

    day 2 part 2

--- solver/day2.ts
+++ solver/day2.ts
@@ -45,7 +45,41 @@ const solver: Solver = {
     return sum.toString();
   },
   solvePart2: () => {
-    return "";
+
+    let sum = 0;
+
+    input.forEach((line) => {
+      let maxRed = 0;
+      let maxGreen = 0;
+      let maxBlue = 0;
+
+      const rounds = line.slice(line.indexOf(": ") + 2).split("; ");
+      rounds.forEach((round) => {
+        const showings = round.split(", ");
+        showings.forEach((show) => {
+          const parts = show.split(" ");
+          const count = parseInt(parts[0]);
+          const color = parts[1];
+
+          if (color == "red") {
+            if (count > maxRed) {
+              maxRed = count;
+            }
+          } else if (color == "green") {
+            if (count > maxGreen) {
+              maxGreen = count;
+            }
+          } else if (color == "blue") {
+            if (count > maxBlue) {
+              maxBlue = count;
+            }
+          }
+        });
+      });
+      sum += maxRed * maxGreen * maxBlue;
+    });
+
+    return sum.toString();
   },
 };
 
